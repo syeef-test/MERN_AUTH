4f8838ced33828c03d02e1a63b6760a8428da991
syeef <syeefislam@yahoo.com>
2025-12-05 20:04:11 +0530

    redis added

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,11 @@
 import { registerSchema } from "../config/zod.js";
 import TryCatch from "../middlewares/TryCatch.js";
 import sanitize from "mongo-sanitize";
+import { redisClient } from "../index.js";
+import { User } from "../models/User.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto"; //inbuilt nodejs
+import {getOtpHtml,getVerifyEmailHtml} from "../config/html.js";
 
 export const registerUser = TryCatch(async (req, res) => {
   const sanitezedBody = sanitize(req.body);
@@ -26,5 +31,45 @@ export const registerUser = TryCatch(async (req, res) => {
 
   const { name, email, password } = validation.data;
 
-  res.json({ name, email, password });
+  const rateLimitKey = `register-rate-limit:${req.ip}:${email}`;
+
+  if (await redisClient.get(rateLimitKey)) {
+    return res.status(429).json({
+      message: "To many requests, try again later",
+    });
+  }
+
+  const existingUser = await User.findOne({
+    email,
+  });
+
+  if (existingUser) {
+    res.status(400).json({ message: "User allready exist" });
+  }
+
+  const hashPassword = await bcrypt.hash(password, 10);
+
+  const verifyToken = crypto.randomBytes(32).toString("hex");
+
+  const verifyKey = `verify:${verifyToken}`;
+
+  const datatoStore = JSON.stringify({
+    name,
+    email,
+    password: hashPassword,
+  });
+
+  await redisClient.set(verifyKey, datatoStore, { EX: 300 });
+
+  const subject = "verify your email for Account creation";
+  const html = getVerifyEmailHtml({email,token});
+
+  await sendMail({ email, subject, html });
+
+  await redisClient.set(rateLimitKey, "true", { EX: 60 });
+
+  res.json({
+    message:
+      "If your email is valid,a verification link has been sent.It will expire in 5 minutes.",
+  });
 });
--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import userRoutes from "./routes/user.js";
+import { createClient } from "redis";
 dotenv.config();
+
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  console.log("Missing redis url");
+  process.exit(1);
+}
+
+export const redisClient = createClient({
+  url: redisUrl,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connected to redis"))
+  .catch(console.error);
 
 const app = express();
 
@@ -12,8 +29,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 //Routes
-app.use("/api/v1",userRoutes);
-
+app.use("/api/v1", userRoutes);
 
 app.listen(PORT, async () => {
   await connectDb();
