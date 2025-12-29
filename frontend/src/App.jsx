087d3512add9bbe517b3defd2469709605ce0a9f
syeef <syeefislam@yahoo.com>
2025-12-29 19:52:25 +0530

    user register done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 import VerifyOtp from "./pages/VerifyOtp";
 import { ToastContainer } from "react-toastify";
 import { AppData } from "./context/AppContext";
 import Loading from "./Loading";
+import Verify from "./pages/Verify";
 
 function App() {
   const { isAuth, loading } = AppData();
@@ -18,7 +20,9 @@ function App() {
           <Routes>
             <Route path="/" element={isAuth ? <Home />:<Login/>} />
             <Route path="/login" element={isAuth ? <Home />:<Login/>} />
+            <Route path="/register" element={isAuth ? <Home />:<Register/>} />
             <Route path="/verifyotp" element={isAuth ? <Home />:<VerifyOtp/>} />
+            <Route path="/token/:token" element={isAuth ? <Home />:<Verify/>} />
           </Routes>
           <ToastContainer />
         </BrowserRouter>
--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { server } from "../config/config";
+import Loading from "../Loading";
 
 function Verify() {
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const params = useParams();
+
+  async function verifyUser() {
+    try {
+      console.log("params",params)
+      const { data } = await axios.post(`${server}/api/v1/verify/${params.token}`);
+      setSuccessMessage(data.message);
+    } catch (error) {
+      console.log("error",error);
+      setErrorMessage(error.response.data.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=>{
+    verifyUser();
+  },[])
+
   return (
-    <div>Verify</div>
-  )
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="w-[200px] m-auto mt-12">
+          {successMessage && (
+            <p className="text-green-500 text-2xl">{successMessage}</p>
+          )}
+          {errorMessage && (
+            <p className="text-red-500 text-2xl">{errorMessage}</p>
+          )}
+        </div>
+      )}
+      
+    </>
+  );
 }
 
-export default Verify
+export default Verify;
